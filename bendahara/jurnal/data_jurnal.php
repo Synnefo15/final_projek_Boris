399e6bbd68fa8c6e64cf4822c780d98409771c41
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2021-11-28 13:16:14 +0700

    rafi com jur

--- bendahara/jurnal/data_jurnal.php
+++ bendahara/jurnal/data_jurnal.php
@@ -1,34 +1,35 @@
 <?php 
-$sqlDebit = $koneksi->query("SELECT SUM(debit) FROM `jurnal`");
-while($data = $sqlDebit->fetch_assoc()){
+$sql = $koneksi->query("SELECT SUM(debit) as 'total debit' FROM `jurnal`");
+while($data = $sql->fetch_assoc()){
     $jumlahDebit=$data['total debit'];
 }
 ?>
+
 <?php  
-$sqlKredit = $koneksi->query("");
-while ($data=$sqlKredit->fetch_assoc()){
+$sql = $koneksi->query("SELECT SUM(kredit) as 'total kredit' FROM `jurnal`");
+while ($data=$sql->fetch_assoc()){
 $jumlahKredit=$data['total kredit'];
 }
 ?>
 
 <div class="alert alert-info alert-dismissible">
     <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-    <h3>
+    <h5>
         <i class="icon fas fa-info"></i> Total Debit
-    </h3>
-    <h2>Jumlah Total:
+    </h5>
+    <h5>Jumlah Total:
         <?php
-        echo $jumlahDebit;
+        echo rupiah($jumlahDebit)
         ?>
-    </h2>
-    <h3>
+    </h5>
+    <h5>
         <i class="icon fas fa-info"></i> Total Kredit
-    </h3>
-    <h2>Jumlah Total:
+    </h5>
+    <h5>Jumlah Total:
         <?php
-        echo $jumlahKredit;
+        echo rupiah($jumlahDebit)
         ?>
-    </h2>
+    </h5>
 
 
 </div>
