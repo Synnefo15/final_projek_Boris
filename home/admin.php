464e2d8f6754d7b0dfcbd16e033ed1dddbb3fcc4
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2021-12-14 13:38:41 +0700

    commit

--- home/admin.php
+++ home/admin.php
@@ -1,1 +1,16 @@
-
+<img src="dist/img/logo_B.jpg" class="rounded mx-auto d-block img-fluid" alt="Cinque Terre"
+    style="width: 20%; height : 20%">
+<div class="container">
+    <div class="card mt-5">
+        <div class="card-header">
+            <h4 class="card-title">SI BORIS</h4>
+        </div>
+        <div class="card-body">
+            <p>Si Boris adalah sebuah website yang dipergunakan untuk membantu usaha penjualan tanaman bonbon floris di
+                Kabupaten
+                Jember</p>
+            <p>Si Boris dapat mengelola data transaksi, mengelola tanaman, dan mengelola supplier dan customer
+            </p>
+        </div>
+    </div>
+</div>
